11eb8ab777e62cfa8a21a0e5413810eb0b082101
Lang Yu <silentbalanceyh@126.com>
2018-02-24 18:23:51 +0800

    Track 1 files into repository.
    - modified config/variables.js
    
    Auto commit by GitBook Editor

--- config/variables.js
+++ config/variables.js
@@ -1,13 +1,12 @@
 const fnEnv = (key) => process.env[key];
 module.exports = {
-    $LANG : fnEnv('APP_LANGUAGE'), // VIE - 系统使用的语言
-    $ENDPOINT : process.env.UI_ENDPOINT, // VIE - 远程地址Api
-    $APP : process.env.UI_APP, // 应用程序名称
-    $LOGIN : process.env.URI_LOGIN, // 系统入口（一般是登陆）
-    $MAIN : process.env.URI_MAIN, // 系统主界面地址,
-    $PATH : process.env.URI_CTX, // Context根路径：重要参数
-    $K_SESSION : process.env.KEY_SESSION, // 存储专用Key前缀
-    $K_EVENT : process.env.KEY_EVENT, // Redux专用Key前缀
-    $DEBUG : process.env.DEV_DEBUG, // 是否DEBUG模式
-    $MAP_KEY : process.env.MAP_KEY
-}
+    $LANG : fnEnv('APP_LANGUAGE'),
+    $ENDPOINT : fnEnv('APP_ENDPOINT'),
+    $APP : fnEnv('APP_NAME'),
+    $PATH : fnEnv('APP_ROUTE'),
+    $K_SESSION : fnEnv('KEY_SESSION'),
+    $K_EVENT :fnEnv('KEY_EVENT'),
+    $DEBUG : fnEnv('DEV_DEBUG'),
+    // 第三方
+    $MAP_KEY : fnEnv('MAP_BAIDU')
+};
