437b1d12e37fcdfc1328b3368158b324ca0fa611
Lang <silentbalanceyh@126.com>
2018-04-14 06:15:33 +0800

    Update

--- shell/run-zero.sh
+++ shell/run-zero.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 基础环境变量
-export PORT=3001                                    # 当前用户使用的端口号
+export PORT=3002                                    # 当前用户使用的端口号
 export APP_LANGUAGE=cn                              # 当前应用使用的语言，如果有多语言则需要在不同语言中启动不同应用进行切换，不建议单库多语言部署
 export APP_ENDPOINT=http://localhost:7000           # 当前应用需要访问的远程EndPoint
 export APP_NAME=vie.app.htl                         # 该应用名称
