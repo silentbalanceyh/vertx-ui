ce5237bf81b94d978cb54d1eb0edf411e2e6cbea
Lang <silentbalanceyh@126.com>
2018-05-06 02:50:23 +0800

    升级窗口配置

--- src/ux/Ux.Dialog.js
+++ src/ux/Ux.Dialog.js
@@ -92,8 +92,7 @@ const _configModal = (reference = {}, key, params, dialog = true) => {
              *      }
              * }
              */
-            const success = modal.success;
-            const error = modal.error;
+            const {success = {}, error = {}} = modal;
             const type = success[key] && !error[key] ? "success" : "error";
             const fun = dialog ? _dialogFun[type] : _messageFun[type];
             let message = "success" === type ? success[key] : error[key];
