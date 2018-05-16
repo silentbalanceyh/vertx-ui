141b786c874c996e1ec6b67ef52dbc109b613799
Lang <silentbalanceyh@126.com>
2018-05-16 10:30:44 +0800

    生成器版本

---
+++ src/components/system/dept/Act.Types.js
@@ -0,0 +1,6 @@
+import Ux from 'ux'
+
+export default {
+    fnDepartmentList: Ux.createAction("/RX/MODULE/DEPARTMENT"),
+    fnDepartment: Ux.createAction("/RX/MODULE/DEPARTMENT/GET")
+}
---
+++ src/components/system/dept/mock/index.js
@@ -0,0 +1,13 @@
+import fnSearch from './fnSearch';
+import fnRead from './fnRead';
+import fnSave from './fnSave';
+import fnAdd from './fnAdd';
+import fnRemove from './fnRemove';
+
+export default {
+    fnSearch,
+    fnRead,
+    fnSave,
+    fnAdd,
+    fnRemove
+}
