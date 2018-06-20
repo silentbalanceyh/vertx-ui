7c6a0da976f8c352580a4235450f4b90d1d550f7
Lang <silentbalanceyh@126.com>
2018-06-20 23:23:08 +0800

    专用

--- src/ux/ai/AI.Input.js
+++ src/ux/ai/AI.Input.js
@@ -1,25 +1,91 @@
 import React from 'react';
-import {Checkbox, DatePicker, Input} from 'antd';
+import {Checkbox, DatePicker, Input, InputNumber, Select, TreeSelect} from 'antd';
 import {ListSelector} from "web";
 import RxAnt from './AI.RxAnt'
 import {Modal} from "antd/lib/index";
 
-const aiInput = (reference, jsx = {}) => {
+const aiInput = (reference, jsx = {}, onChange) => {
     // 处理prefix属性
     RxAnt.onPrefix(jsx);
+    // 处理addonAfter
+    RxAnt.onAddonAfter(jsx);
+    // onChange处理
+    RxAnt.onChange(jsx, onChange);
     return (<Input {...jsx}/>)
 };
-const aiCheckbox = (reference, jsx = {}) => {
-    const {$config = {}, ...rest} = jsx;
+
+const aiTreeSelect = (reference, jsx = {}, onChange) => {
+    const {config = {}, ...rest} = jsx;
+    // 处理TreeSelect
+    const data = RxAnt.toTreeOptions(reference, config);
+    // 处理onChange
+    RxAnt.onChange(rest, onChange);
+    return (<TreeSelect treeData={data} {...rest}/>)
+};
+
+const aiInputNumber = (reference, jsx = {}, onChange) => {
+    // onChange处理
+    RxAnt.onChange(jsx, onChange);
+    return (<InputNumber {...jsx}/>)
+};
+const aiSelect = (reference, jsx = {}, onChange) => {
+    const {config = {}, ...rest} = jsx;
+    // onChange处理
+    RxAnt.onChange(jsx, onChange);
+    const options = RxAnt.toOptions(reference, config);
+    return (
+        <Select {...rest}>
+            {options.map(item => (
+                <Select.Option key={item.key} value={item.value}>
+                    {item.label}
+                </Select.Option>
+            ))}
+        </Select>
+    )
+};
+const aiCheckbox = (reference, jsx = {}, onChange) => {
+    const {config, ...rest} = jsx;
     // 构造Checkbox专用选项
-    const options = RxAnt.toOptions(reference, $config);
-    return <Checkbox.Group {...rest} options={options}/>
+    RxAnt.onChange(rest, onChange);
+    const options = RxAnt.toOptions(reference, config);
+    return (config) ?
+        <Checkbox.Group {...rest} options={options}/> :
+        <Checkbox {...jsx}/>
 };
 const aiTextArea = (reference, jsx = {}) => {
     return (<Input.TextArea {...jsx}/>)
 };
-const aiDatePicker = (reference, jsx = {}) => {
+const aiDatePicker = (reference, jsx = {}, onChange) => {
+    // DisabledDate
+    RxAnt.onDisabledDate(jsx);
+    // onChange处理
+    RxAnt.onChange(jsx, onChange);
     return (<DatePicker {...jsx}/>);
+};
+
+const ai2Select = (onChange) => (reference, jsx = {}) => {
+    const fnChange = onChange.apply(null, [reference]);
+    return aiSelect(reference, jsx, fnChange);
+};
+
+const ai2TreeSelect = (onChange) => (reference, jsx = {}) => {
+    const fnChange = onChange.apply(null, [reference]);
+    return aiTreeSelect(reference, jsx, fnChange);
+};
+
+const ai2DatePicker = (onChange) => (reference, jsx = {}) => {
+    const fnChange = onChange.apply(null, [reference]);
+    return aiDatePicker(reference, jsx, fnChange);
+};
+
+const ai2Checkbox = (onChange) => (reference, jsx = {}) => {
+    const fnChange = onChange.apply(null, [reference]);
+    return aiCheckbox(reference, jsx, fnChange);
+};
+
+const ai2InputNumber = (onChange) => (reference, jsx = {}) => {
+    const fnChange = onChange.apply(null, [reference]);
+    return aiInputNumber(reference, jsx, fnChange);
 };
 
 const ai2ListSelector = (mockData = {}) => (reference, jsx = {}) => {
@@ -36,10 +102,18 @@ export default {
     // 对话框专用
     aiConfirm,
     // 直接组件
+    aiSelect,
     aiInput,
+    aiInputNumber,
     aiCheckbox,
     aiTextArea,
+    aiTreeSelect,
     aiDatePicker,
     // 二阶函数
+    ai2Checkbox,
+    ai2DatePicker,
+    ai2InputNumber,
+    ai2TreeSelect,
+    ai2Select,
     ai2ListSelector
 }
--- src/ux/ai/AI.RxAnt.js
+++ src/ux/ai/AI.RxAnt.js
@@ -3,6 +3,8 @@ import {Icon} from 'antd';
 import U from "underscore";
 import Prop from "../Ux.Prop";
 import Random from "../Ux.Random";
+import Attributes from '../Ux.Attribute';
+import Uarr from '../structure/Ux.Uarr'
 
 const uniform = (item, callback) => {
     if (!callback || !U.isFunction(callback)) {
@@ -25,27 +27,81 @@ const applyValue = (option) => {
     })
 };
 
+const applyIcon = (jsx, key = "") => {
+    const {type, ...rest} = jsx[key];
+    jsx[key] = (
+        <Icon type={type} {...rest}/>
+    )
+};
+
+const parseExpr = (expr = "") => {
+    const item = expr.replace(/ /g, '');
+    const kv = item.split(',');
+    const attr = {};
+    kv.forEach(keyValue => {
+        const key = keyValue.split('=')[0];
+        attr[key] = keyValue.split('=')[1];
+    });
+    if (!attr.hasOwnProperty('key')) {
+        attr.key = Random.randomString(12);
+    }
+    return attr;
+};
+
+const parseDatum = (config = {}) => {
+    let meta = config.datum;
+    if ("string" === typeof config.datum) {
+        meta = parseExpr(config.datum);
+    }
+    return meta;
+};
+
+const extractDatum = (reference, config = {}) => {
+    let options = [];
+    // 如果存在datum节点，则从Assist/Tabular数据源中读取
+    const {source} = parseDatum(config);
+    if (source && "string" === typeof source) {
+        const data = Prop.onDatum(reference, source);
+        if (U.isArray(data)) {
+            options = data;
+        } else {
+            console.error(`[RxAnt] The 'source=${source}' data must be Array.`);
+        }
+    } else {
+        console.error("[RxAnt] The 'source' key of configuration is invalid.");
+    }
+    return options;
+};
+
 class RxAnt {
     static toParsed(expr = "") {
-        const item = expr.replace(/ /g, '');
-        const kv = item.split(',');
-        const attr = {};
-        kv.forEach(keyValue => {
-            const key = keyValue.split('=')[0];
-            attr[key] = keyValue.split('=')[1];
-        });
-        if (!attr.hasOwnProperty('key')) {
-            attr.key = Random.randomString(12);
+        return parseExpr(expr);
+    }
+
+    static onDisabledDate(jsx = {}) {
+        if (jsx.hasOwnProperty('disabledDate')) {
+            const attrFun = Attributes[jsx.disabledDate];
+            if (U.isFunction(attrFun)) {
+                jsx.disabledDate = attrFun;
+            }
         }
-        return attr;
     }
 
     static onPrefix(jsx = {}) {
         if ("object" === typeof jsx.prefix) {
-            const {type, ...rest} = jsx.prefix;
-            jsx.prefix = (
-                <Icon type={type} {...rest}/>
-            )
+            applyIcon(jsx, 'prefix');
+        }
+    }
+
+    static onAddonAfter(jsx = {}) {
+        if ("object" === typeof jsx.addonAfter) {
+            applyIcon(jsx, 'addonAfter');
+        }
+    }
+
+    static onChange(jsx = {}, onChange) {
+        if (U.isFunction(onChange)) {
+            jsx.onChange = onChange;
         }
     }
 
@@ -61,34 +117,47 @@ class RxAnt {
         }
     }
 
-    static toOptions(reference, $config = {}) {
+    static toTreeOptions(reference, config = {}) {
         let options = [];
-        if ($config.items) {
+        if (config.items) {
+            options = config.items;
+        } else if (config.datum) {
+            options = extractDatum(reference, config);
+        }
+        const treeData = Uarr.create(options)
+            .sort((left, right) => left.left - right.left)
+            .convert("code", (code, item) => item["code"] + " - " + item["name"])
+            .mapping({
+                id: "id",
+                pid: "pid",
+                label: "code",
+                value: "id"
+            })
+            .tree("id", "pid")
+            .to();
+        return treeData;
+    }
+
+    static toOptions(reference, config = {}) {
+        let options = [];
+        if (config.items) {
             // 如果存在items的根节点，则直接items处理
-            options = $config.items;
-        } else if ($config.datum) {
+            options = config.items;
+        } else if (config.datum) {
             // 如果存在datum节点，则从Assist/Tabular数据源中读取
-            const {source, key = "key", label = "label"} = $config.datum;
-            if (source && "string" === typeof source) {
-                const data = Prop.onDatum(reference, source);
-                if (U.isArray(data)) {
-                    data.forEach(each => {
-                        const option = {};
-                        if (each[key]) {
-                            option['value'] = each[key];
-                            option['key'] = each[key];
-                        }
-                        if (each[label]) {
-                            option['label'] = each[label];
-                        }
-                        options.push(option);
-                    })
-                } else {
-                    console.error(`[RxAnt] The 'source=${source}' data must be Array.`);
+            const data = extractDatum(reference, config);
+            const {key = "key", label = "label"} = parseDatum(config);
+            data.forEach(each => {
+                const option = {};
+                if (each[key]) {
+                    option['value'] = each[key];
+                    option['key'] = each[key];
                 }
-            } else {
-                console.error("[RxAnt] The 'source' key of configuration is invalid.");
-            }
+                if (each[label]) {
+                    option['label'] = each[label];
+                }
+                options.push(option);
+            });
         }
         applyValue(options);
         return options;
