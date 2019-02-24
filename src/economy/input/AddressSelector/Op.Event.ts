256048b2d2b739d6da51ef4dcb2a4fa0f4bb00cc
Lang <silentbalanceyh@126.com>
2019-02-24 23:30:18 +0800

    Address Selector

--- src/economy/input/AddressSelector/Op.Event.ts
+++ src/economy/input/AddressSelector/Op.Event.ts
@@ -8,17 +8,17 @@ const bindChange = (reference: any) => (value, selectedOptions) => {
         if (updated) {
             // 选择触发时，country / state / city 整个一条链上的数据已经加载完成，这里value为数组，直接设置
             reference.setState({defaultValue: Ux.clone(value)});
-            const {onChange, onPostChange} = reference.props;
+            const {onChange, rxSelect} = reference.props;
             if (U.isFunction(onChange)) {
                 // 表单设置值
                 Ux.dgDebug({updated}, "表单更改值 = ");
                 // 将当前值更新为distinctId专用
                 onChange(updated);
             }
-            if (U.isFunction(onPostChange)) {
+            if (U.isFunction(rxSelect)) {
                 // 更改完成过后的变更函数
                 const {options = []} = reference.state;
-                onPostChange(value, options);
+                rxSelect(value, options);
             }
         }
     }
