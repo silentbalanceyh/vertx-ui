7ec60e610343abd9acff6ffa722fcbffad72612e
Lang <silentbalanceyh@126.com>
2019-07-16 13:27:02 +0800

    新版可运行

--- src/economy/backup/o-designer/FormDesigner/UI.js
+++ src/economy/backup/o-designer/FormDesigner/UI.js
@@ -4,7 +4,7 @@ import Toolbar from './UI.Toolbar';
 import Topbar from './UI.Topbar';
 import Layout from './UI.Layout';
 import HTML5Backend from 'react-dnd-html5-backend';
-import {DragDropContext} from 'react-dnd';
+import {DndProvider} from 'react-dnd';
 
 class Component extends React.PureComponent {
     constructor(props) {
@@ -16,17 +16,19 @@ class Component extends React.PureComponent {
 
     render() {
         return (
-            <div>
-                {Ux.aiRows(["web-layout-row", ""],
-                    <Topbar/>,
-                    Ux.aiGrid([21, 3],
-                        <Layout pointer={this} target={this.state.mounted}/>,
-                        <Toolbar pointer={this}/>
-                    )
-                )}
-            </div>
+            <DndProvider backend={HTML5Backend}>
+                <div>
+                    {Ux.aiRows(["web-layout-row", ""],
+                        <Topbar/>,
+                        Ux.aiGrid([21, 3],
+                            <Layout pointer={this} target={this.state.mounted}/>,
+                            <Toolbar pointer={this}/>
+                        )
+                    )}
+                </div>
+            </DndProvider>
         );
     }
 }
 
-export default DragDropContext(HTML5Backend)(Component);
+export default Component;
