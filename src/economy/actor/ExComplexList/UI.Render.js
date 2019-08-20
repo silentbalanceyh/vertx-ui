3517ff7adf6a3411cc44d7c4d8b3c858cfdff6f9
Lang <silentbalanceyh@126.com>
2019-08-20 17:51:53 +0800

    旧内容删除

--- src/economy/actor/ExComplexList/UI.Render.js
+++
@@ -1,9 +0,0 @@
-import PageList from './UI.Page.List';
-import PageAdd from './UI.Page.Add';
-import PageEdit from './UI.Page.Edit';
-
-export default {
-    "list": PageList,
-    "add": PageAdd,
-    "edit": PageEdit
-};
