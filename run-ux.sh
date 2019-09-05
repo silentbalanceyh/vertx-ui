4cab58eb3fce679b534b4351185c9843560bea3c
Lang <silentbalanceyh@126.com>
2019-09-05 18:13:11 +0800

    新的分发脚本

--- run-ux.sh
+++ run-ux.sh
@@ -1,49 +1,20 @@
 #!/usr/bin/env bash
 
-# export FOLDER="/Users/lang/Develop/Source/uniform-ui/vie-ui"
-# 拷贝
-# cp -r src/entity/* ${FOLDER}/src/entity
-# cp -r src/economy/* ${FOLDER}/src/economy
-# cp -r src/ux/* ${FOLDER}/src/ux
-# echo "[ ${FOLDER} ] 拷贝完成！"
-# export FOLDER="/Users/lang/Develop/Work/Source/ima-app"
-# 拷贝
-# cp -r src/entity/* ${FOLDER}/src/entity
-# cp -r src/economy/* ${FOLDER}/src/economy
-# cp -r src/ux/* ${FOLDER}/src/ux
-# echo "[ ${FOLDER} ] 拷贝完成！"
-# export TARGET_FOLDER=/Users/lang/Develop/Source/uniform-ui/ima-app/src
-# 北京一体化项目
-# cp -rf src/environment/* ${TARGET_FOLDER}/environment/
-# cp -rf src/economy/* ${TARGET_FOLDER}/economy/
-# cp -rf src/entity/* ${TARGET_FOLDER}/entity/
-# cp -rf src/ux/* ${TARGET_FOLDER}/ux/
-# Zero扩展模块
-# cp -rf src/extension/* ${TARGET_FOLDER}/extension/
-# cp -rf src/carol/* ${TARGET_FOLDER}/carol/
-# 资源文件
-# cp -rf src/cab/cn/shared.json ${TARGET_FOLDER}/cab/cn/shared.json
-# cp -rf src/cab/cn/economy/* ${TARGET_FOLDER}/cab/cn/economy/
-# cp -rf src/environment/zero.js ${TARGET_FOLDER}/environment/zero.js
-# Zero扩展模块
-# cp -rf src/cab/cn/extension/* ${TARGET_FOLDER}/cab/cn/extension/
-# Others
-# echo "[ ${TARGET_FOLDER} ] 拷贝完成！"
 export TARGET_FOLDER=/Users/lang/Develop/Source/uniform-ui/vie-ui/src
-# cp -rf src/environment/* ${TARGET_FOLDER}/environment/
+# 先删除
 cp -rf src/economy/* ${TARGET_FOLDER}/economy/
 cp -rf src/entity/* ${TARGET_FOLDER}/entity/
 cp -rf src/ux/* ${TARGET_FOLDER}/ux/
-# Zero扩展模块
 cp -rf src/extension/* ${TARGET_FOLDER}/extension/
 echo "[ ${TARGET_FOLDER} ] 拷贝完成！"
 # 资源文件
 cp -rf src/cab/cn/shared.json ${TARGET_FOLDER}/cab/cn/shared.json
 cp -rf src/cab/cn/economy/* ${TARGET_FOLDER}/cab/cn/economy/
 cp -rf src/environment/zero.js ${TARGET_FOLDER}/environment/zero.js
-# Zero扩展模块
+cp -rf src/environment/zero.fn.js ${TARGET_FOLDER}/environment/zero.fn.js
 cp -rf src/cab/cn/extension/* ${TARGET_FOLDER}/cab/cn/extension/
-# Others
+
+
 export TARGET_FOLDER=/Users/lang/Develop/Source/uniform-ui/ox-ui/src
 # cp -rf src/environment/* ${TARGET_FOLDER}/environment/
 cp -rf src/economy/* ${TARGET_FOLDER}/economy/
@@ -56,6 +27,7 @@ echo "[ ${TARGET_FOLDER} ] 拷贝完成！"
 cp -rf src/cab/cn/shared.json ${TARGET_FOLDER}/cab/cn/shared.json
 cp -rf src/cab/cn/economy/* ${TARGET_FOLDER}/cab/cn/economy/
 cp -rf src/environment/zero.js ${TARGET_FOLDER}/environment/zero.js
+cp -rf src/environment/zero.fn.js ${TARGET_FOLDER}/environment/zero.fn.js
 # Zero扩展模块
-cp -rf src/cab/cn/extension/* ${TARGET_FOLDER}/cab/cn/extension/tatu
+cp -rf src/cab/cn/extension/* ${TARGET_FOLDER}/cab/cn/extension/
 
