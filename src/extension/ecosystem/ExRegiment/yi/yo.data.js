947152f9eef013ab4561aa97dee6118e3f96bd12
Lang <silentbalanceyh@126.com>
2019-11-21 06:09:00 +0800

    重新开发组件

--- src/extension/ecosystem/ExRegiment/yi/yo.data.js
+++
@@ -1,8 +0,0 @@
-import Ux from 'ux';
-
-export default (reference) => {
-    const {$data = [], $selected = []} =
-        reference.state ? reference.state : {};
-    const $keys = Ux.immutable($selected.map(each => each.key));
-    return $data.filter(each => !$keys.contains(each.key));
-}
