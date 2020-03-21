95bfe0ae8d99ba422b4a0ad90d19521ffb689080
Lang <silentbalanceyh@126.com>
2020-03-21 08:46:15 +0800

    story book

---
+++ run-story.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+export PORT=4000
+export Z_TITLE=Zero教程文档说明
+export Z_LANGUAGE=cn
+export Z_ENDPOINT=http://localhost:7000
+export Z_APP=vie.app.htl
+export Z_ROUTE=zui
+export Z_SHARED=app
+export Z_ENTRY_LOGIN=/login/index
+export Z_ENTRY_ADMIN=/admin/index
+export Z_K_SESSION=@@ZUI/
+export Z_K_EVENT=@@ZUI-ACT
+export REACT_APP_LESS_MODULES=true
+
+export Z_DEV_DEBUG=true
+export Z_DEV_MOCK=true
+export Z_DEV_AJAX=false
+export Z_DEV_FORM=true
+export Z_DEV_MONITOR=false
+
+export Z_CSS_COLOR="#3457e2"
+export Z_CSS_PREFIX=zui
+
+export SB_PORT=6006
+npm run-script build-storybook
+npm run-script storybook
--- stories/economy/loading/LoadingContent.stories.js
+++ stories/economy/loading/LoadingContent.stories.js
@@ -3,9 +3,9 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {linkTo} from '@storybook/addon-links';
 import {Welcome} from '@storybook/react/demo';
-import {LoadingContent} from 'web';
 
-storiesOf('Welcome', module).add('LoadingContent', () => <Welcome showApp={linkTo('LoadingContent')}/>);
+storiesOf('Welcome', module)
+    .add('LoadingContent', () => <Welcome showApp={linkTo('LoadingContent')}/>);
 
 storiesOf('LoadingContent', module)
     .add('with text', () => <div>Hello Button</div>);
