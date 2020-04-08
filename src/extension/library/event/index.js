68fe88bfdd4994e2280d56e6751e6d7af3847ba2
Lang <silentbalanceyh@126.com>
2020-04-08 18:13:27 +0800

    Process Document

--- src/extension/library/event/index.js
+++ src/extension/library/event/index.js
@@ -1,6 +1,19 @@
 import upstream from './upstream';
 import fabric from './fabric';
-
+/**
+ * 内部模块
+ *
+ * rx系列方法
+ *
+ * @module _rx
+ */
+/**
+ * 内部模块
+ *
+ * 事件调度方法
+ *
+ * @module _event
+ */
 export default {
     ...upstream,
     ...fabric,
