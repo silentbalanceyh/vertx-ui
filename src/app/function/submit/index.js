0c2d43bb90a8eb826690f3706569743e0bbe8ce9
Lang <silentbalanceyh@126.com>
2020-05-22 15:05:18 +0800

    Process

--- src/app/function/submit/index.js
+++ src/app/function/submit/index.js
@@ -6,7 +6,13 @@ const _opSubmit = (reference) => (params = {}) => {
     reference.setState({$loading: false, $submitting: false});
     return Ux.promise(params);
 };
+const _opReset = (reference) => (event) => {
+    const ref = Ux.onReference(reference, 2);
+    ref.setState({json: {}});
+    Ux.formReset(ref);
+}
 export default {
     $opAdd: _opSubmit,
-    $opSave: _opSubmit
+    $opSave: _opSubmit,
+    $opReset: _opReset,
 }
