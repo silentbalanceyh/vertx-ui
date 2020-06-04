f799a69bfaa2ec602d95c721c6bda6f67ed972a1
Lang <silentbalanceyh@126.com>
2020-06-04 20:46:59 +0800

    依赖规则

--- src/economy/input/InputArray/Op.js
+++ src/economy/input/InputArray/Op.js
@@ -59,12 +59,11 @@ const rxChange = (reference, index) => (event) => {
 }
 const isDisabled = (reference) => {
     const {
-        config = {}, disabled = false
+        config = {}
     } = reference.props;
     const {$holder = 0} = reference.state;
     const {limit = -1} = config;
     /* 如果 > 0 就检查 */
-    console.info(disabled);
     if (0 < limit) {
         return ($holder >= limit);
     } else return false;
