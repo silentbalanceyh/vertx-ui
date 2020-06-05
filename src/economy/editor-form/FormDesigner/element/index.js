b8fe4e78075233522cc31d2533fa86d726442983
Lang <silentbalanceyh@126.com>
2020-06-05 15:09:16 +0800

    处理

--- src/economy/editor-form/FormDesigner/element/index.js
+++ src/economy/editor-form/FormDesigner/element/index.js
@@ -1,9 +1,11 @@
 import aiInput from './aiInput';
 import aiInputNumber from "./aiInputNumber";
 import aiPassword from "./aiPassword";
+import aiTextArea from "./aiTextArea";
 
 export default {
     aiInput,
+    aiPassword,
     aiInputNumber,
-    aiPassword
+    aiTextArea
 }
