042039ed7d3091515fded3aec59a68c4b319074c
Lang <silentbalanceyh@126.com>
2021-03-04 11:05:51 +0800

    Ex库重构完成

--- src/extension/library/event/fabric/processor/DIALOG.js
+++
@@ -1,8 +0,0 @@
-export default {
-    VISIBLE: (params = []) => async (dataEvent) => {
-        const normalized = {};
-        normalized.$visible = true;
-        normalized.$current = dataEvent.getPrev();
-        return normalized;
-    }
-}
