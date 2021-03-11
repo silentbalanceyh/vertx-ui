3eaa5c6e226de7c39bb0d37b33462769943c860f
Lang <silentbalanceyh@126.com>
2021-03-11 17:17:27 +0800

    web-unit

--- src/ux/engine/web-unit.js
+++ src/ux/engine/web-unit.js
@@ -571,6 +571,42 @@ const aiAnchor = (item = {}, onClick, defaultType = "BUTTON") => {
         return false;
     }
 }
+const aiCardLeft = (reference, buttons = [], disabled = {}) => {
+    return (
+        <Button.Group>
+            {buttons.map(button => {
+                const $item = Abs.clone(button);
+                /*
+                 * Redux 专用处理
+                 */
+                const {$submitting} = reference.props;
+                if ($submitting && $submitting.is()) {
+                    const submitting = $submitting.to();
+                    $item.loading = submitting.loading;
+                } else {
+                    if ("boolean" === typeof $submitting) {
+                        $item.loading = $submitting;
+                    }
+                }
+                if (disabled) {
+                    if (Abs.isObject(disabled)) {
+                        if (disabled.hasOwnProperty($item.key)) {
+                            $item.disabled = disabled[$item.key];
+                        }
+                    } else if (true === disabled) {
+                        $item.disabled = disabled;
+                    }
+                } else {
+                    $item.disabled = false;
+                }
+                const {text, ...rest} = $item;
+                return (
+                    <Button {...rest}>{text}</Button>
+                )
+            })}
+        </Button.Group>
+    )
+}
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     aiFloatError,
@@ -590,4 +626,6 @@ export default {
     aiBlock,    // 上边文字 / 下边图标
     aiEmpty,    // 空处理
     aiAnchor,   // 连接 / 按钮 双执行处理
+
+    aiCardLeft, // PageCard左侧专用
 }
