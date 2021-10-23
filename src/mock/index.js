69e0296d27124ad90e2a323ec7a957023a02f03d
Lang <silentbalanceyh@126.com>
2021-10-23 14:17:09 +0800

    主页显示

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,21 @@
 // Zero Framework全Mock接口
-import get_app_name_$name from './zero-ui/get_app_name_$name';
+import get_app_name_$name from './zero-ui/login/get_app_name_$name';
+import post_oauth_login from "./zero-ui/login/post_oauth_login";
+import post_oauth_token from "./zero-ui/login/post_oauth_token";
+import post_oauth_authorize from "./zero-ui/login/post_oauth_authorize";
+import get_api_user from "./zero-ui/login/get_api_user";
+import get_api_app from "./zero-ui/login/get_api_app";
+import get_api_menus from './zero-ui/login/get_api_menus';
+import post_api_user_logout from './zero-ui/login/post_api_user_logout';
 
 export default {
-    get_app_name_$name
+    // 登录到主界面
+    get_app_name_$name,
+    post_oauth_login,
+    post_oauth_authorize,
+    post_oauth_token,
+    get_api_user,
+    get_api_app,
+    get_api_menus,
+    post_api_user_logout,
 }
---
+++ src/mock/zero-ui/login/get_api_app.js
@@ -0,0 +1,20 @@
+export default {
+    mock: true,
+    data: {
+        "key": "78fce5a2-17f3-4dac-a75c-7e751595015c",
+        "name": "vie.app.zui",
+        "code": "zui",
+        "title": "Zero框架脚手架",
+        "domain": "localhost",
+        "appPort": 5100,
+        "urlEntry": "/login/index",
+        "urlMain": "/main/index",
+        "path": "/zui",
+        "route": "/zui",
+        "appKey": "18VqhtJUCgHe1N0pgq1wrl7BcC5FFE0fcOJ8e2XO8ShqRIkOysdt970XzNRc0eFc",
+        "active": true,
+        "sigma": "ENhwBAJPZuSgIAE5EDakR6yrIQbOoOPq",
+        "language": "cn",
+        "createdBy": "zero-environment"
+    }
+}
