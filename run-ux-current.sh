65b6c84c0355fda75297577498e872c29176fd6e
Lang <silentbalanceyh@126.com>
2022-02-15 08:51:54 +0800

    current

--- run-ux-current.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-./run-ux.sh /Users/lang/Develop/Source/uniform-ui/hotel-ui
-./run-ux.sh /Users/lang/Develop/Source/uniform-ui/ox-ui
-./run-ux.sh /Users/lang/Develop/Source/uniform-ui/xc-ui
-./run-ux.sh /Users/lang/Develop/Source/uniform-ui/tm-ui
